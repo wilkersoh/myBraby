5ad97e5d29b01480c4e8aee6ecc671fa3135e513
wilkersoh <plgu@hotmail.com>
2019-04-19 15:15:46 +0800

    filePond no working

--- public/javascripts/fileUploader.js
+++ public/javascripts/fileUploader.js
@@ -1,10 +1,15 @@
+    
 FilePond.registerPlugin(
     FilePondPluginImagePreview,
     FilePondPluginImageResize,
     FilePondPluginFileEncode,
-)
-
-
-FilePond.parse(document.body);
-
+  )
+  
+  FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+  })
+  
+  FilePond.parse(document.body);
 
